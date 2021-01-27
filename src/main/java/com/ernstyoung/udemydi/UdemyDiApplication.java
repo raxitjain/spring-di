4a0968b28f7544c5e0823455541f5908e356d605
Raxit Jain <rjain@nvenergy.com>
2021-01-28 00:05:39 +0530

    Implemented fake data source by externalizing properties

--- src/main/java/com/ernstyoung/udemydi/UdemyDiApplication.java
+++ src/main/java/com/ernstyoung/udemydi/UdemyDiApplication.java
@@ -1,6 +1,7 @@
 package com.ernstyoung.udemydi;
 
 import com.ernstyoung.udemydi.controllers.*;
+import com.ernstyoung.udemydi.models.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,11 +12,14 @@ public class UdemyDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(UdemyDiApplication.class, args);
 
-		MyController controller = (MyController) applicationContext.getBean("myController");
-		System.out.println(controller.sayHello());
-		System.out.println(applicationContext.getBean(PropertyInjectedController.class).getGreeting());
-		System.out.println(applicationContext.getBean(SetterInjectedController.class).getGreeting());
-		System.out.println(applicationContext.getBean(ConstructorInjectedController.class).getGreeting());
+		FakeDataSource fakeDataSource;
+		fakeDataSource = (FakeDataSource) applicationContext.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUsername() + " " + fakeDataSource.getPassword());
+//		MyController controller = (MyController) applicationContext.getBean("myController");
+//		System.out.println(controller.sayHello());
+//		System.out.println(applicationContext.getBean(PropertyInjectedController.class).getGreeting());
+//		System.out.println(applicationContext.getBean(SetterInjectedController.class).getGreeting());
+//		System.out.println(applicationContext.getBean(ConstructorInjectedController.class).getGreeting());
 	}
 
 }
---
+++ src/main/java/com/ernstyoung/udemydi/config/PropertyConfig.java
@@ -0,0 +1,37 @@
+package com.ernstyoung.udemydi.config;
+
+import com.ernstyoung.udemydi.models.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${raxit.username}")
+    String username;
+
+    @Value("${raxit.password}")
+    String password;
+
+    @Value("${raxit.url}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
