78a5e8aa1a1827514bfecaa2d1168071aac2d86e
Raxit Jain <rjain@nvenergy.com>
2021-01-28 12:15:47 +0530

    Add multiple property files and handled in configuration class

--- src/main/java/com/ernstyoung/udemydi/UdemyDiApplication.java
+++ src/main/java/com/ernstyoung/udemydi/UdemyDiApplication.java
@@ -2,6 +2,7 @@ package com.ernstyoung.udemydi;
 
 import com.ernstyoung.udemydi.controllers.*;
 import com.ernstyoung.udemydi.models.FakeDataSource;
+import com.ernstyoung.udemydi.models.FakeJMSBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +15,8 @@ public class UdemyDiApplication {
 
 		FakeDataSource fakeDataSource = (FakeDataSource) applicationContext.getBean(FakeDataSource.class);
 		System.out.println(fakeDataSource.getUsername() + " " + fakeDataSource.getPassword() + " " + fakeDataSource.getUrl());
+		FakeJMSBroker fakeJMSBroker = (FakeJMSBroker) applicationContext.getBean(FakeJMSBroker.class);
+		System.out.println(fakeJMSBroker.getUsername() + " " + fakeJMSBroker.getPassword() + " " + fakeJMSBroker.getUrl());
 //		MyController controller = (MyController) applicationContext.getBean("myController");
 //		System.out.println(controller.sayHello());
 //		System.out.println(applicationContext.getBean(PropertyInjectedController.class).getGreeting());
--- src/main/java/com/ernstyoung/udemydi/config/PropertyConfig.java
+++ src/main/java/com/ernstyoung/udemydi/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package com.ernstyoung.udemydi.config;
 
 import com.ernstyoung.udemydi.models.FakeDataSource;
+import com.ernstyoung.udemydi.models.FakeJMSBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -19,6 +25,9 @@ public class PropertyConfig {
     @Value("${raxit.username}") String username;
     @Value("${raxit.password}") String password;
     @Value("${raxit.url}") String url;
+    @Value("${jms.username}") String jmsUsername;
+    @Value("${jms.password}") String jmsPassword;
+    @Value("${jms.url}") String jmsUrl;
 
     @Bean
     public FakeDataSource fakeDataSource() {
@@ -27,6 +36,15 @@ public class PropertyConfig {
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJMSBroker fakeJMSBroker() {
+        FakeJMSBroker fakeJMSBroker = new FakeJMSBroker();
+        fakeJMSBroker.setUsername(jmsUsername);
+        fakeJMSBroker.setPassword(jmsPassword);
+        fakeJMSBroker.setUrl(jmsUrl);
+        return fakeJMSBroker;
     }
 
     @Bean
